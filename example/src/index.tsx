ea4b3633fab9a329a7e0a9e6ebbb6ed89cfb0a8f
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-09-29 09:49:13 -0700

    Fixed Prettier

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import {createRoot} from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 
-import {Button} from '@/index'
+import { Button } from '@/index'
 
 const App = () => {
     return (
@@ -15,4 +15,8 @@ const App = () => {
 const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container)
 
-root.render(<React.StrictMode><App /></React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 
-import {Button} from '@/index'
+import { Button } from '@/index'
 
 const App = () => (
     <div>
