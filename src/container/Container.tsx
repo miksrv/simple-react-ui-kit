76163984fdf964ad49f23ff74875a1822800b3fa
Misha Topchilo <miksoft.tm@gmail.com>
2024-10-02 10:09:46 -0700

    Added descriptions for Container Props UI Component (#5)
    * Added descriptions for Container Props UI Component
    
    * Improved CI/CD Actions
    
    * Update checks.yml
    
    ---------
    
    Co-authored-by: Misha Topchilo <20854498+miksrv@users.noreply.github.com>

--- src/container/Container.tsx
+++ src/container/Container.tsx
@@ -4,12 +4,21 @@ import styles from './styles.module.sass'
 
 import { concatClassNames as cn } from '@/tools'
 
+/**
+ * Interface for the properties of a container component.
+ */
 export interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {
+    /** The title of the container. Displayed at the top of the container */
     title?: string
+    /** Additional class names to apply to the container */
     className?: string
+    /** An action element, typically a button or link, displayed in the container header */
     action?: React.ReactNode
+    /** Custom header content for the container */
     header?: React.ReactNode
+    /** The content of the container */
     children?: React.ReactNode
+    /** Custom footer content for the container */
     footer?: React.ReactNode
 }
 
