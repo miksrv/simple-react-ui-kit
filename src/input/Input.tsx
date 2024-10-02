216fb80c32bbf35d32f5f0ae81218cfba002b232
Misha Topchilo <miksoft.tm@gmail.com>
2024-10-02 11:53:57 -0700

    Testing publish new UI version (#8)
    Co-authored-by: Misha Topchilo <20854498+miksrv@users.noreply.github.com>

--- src/input/Input.tsx
+++ src/input/Input.tsx
@@ -4,26 +4,11 @@ import styles from './styles.module.sass'
 
 import { concatClassNames as cn } from '@/tools'
 
-/**
- * Props for the Input component.
- *
- * @interface InputProps
- * @extends {React.InputHTMLAttributes<HTMLInputElement>}
- * @property {string} [label] - The label for the input field.
- * @property {string} [error] - Error message to display below the input.
- */
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
     label?: string
     error?: string
 }
 
-/**
- * A functional component that renders an input field with an optional label and error message.
- * @param label
- * @param error
- * @param props
- * @returns {JSX.Element} The rendered input component.
- */
 const Input: React.FC<InputProps> = ({ label, error, ...props }: InputProps): JSX.Element => (
     <div
         className={cn(
