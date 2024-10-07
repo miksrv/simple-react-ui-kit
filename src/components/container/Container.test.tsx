cacdf3eabb0c9ca2430380b54f2b92a71c9b030c
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-10-07 11:16:11 -0700

    Added new story and UI tests for Container

---
+++ src/components/container/Container.test.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+import '@testing-library/jest-dom'
+
+import Container, { ContainerProps } from './Container'
+
+import { render, screen } from '@testing-library/react'
+
+describe('Container Component', () => {
+    const defaultProps: ContainerProps = {
+        title: 'Test Title',
+        children: 'This is a container content',
+        footer: <div>Footer Content</div>
+    }
+
+    it('renders the container with title and content', () => {
+        render(<Container {...defaultProps} />)
+        const titleElement = screen.getByText(/Test Title/i)
+        const contentElement = screen.getByText(/This is a container content/i)
+        expect(titleElement).toBeInTheDocument()
+        expect(contentElement).toBeInTheDocument()
+    })
+
+    it('renders the footer if provided', () => {
+        render(<Container {...defaultProps} />)
+        const footerElement = screen.getByText(/Footer Content/i)
+        expect(footerElement).toBeInTheDocument()
+    })
+
+    it('applies additional class names', () => {
+        const { container } = render(
+            <Container
+                {...defaultProps}
+                className='custom-container-class'
+            />
+        )
+        const containerElement = container.querySelector('section')
+        expect(containerElement).toHaveClass('custom-container-class')
+    })
+
+    it('renders custom header when provided', () => {
+        const customHeader = <div>Custom Header</div>
+        render(
+            <Container
+                {...defaultProps}
+                header={customHeader}
+            />
+        )
+        const headerElement = screen.getByText(/Custom Header/i)
+        expect(headerElement).toBeInTheDocument()
+    })
+
+    it('renders an action element if provided', () => {
+        const actionElement = <button>Action Button</button>
+        render(
+            <Container
+                {...defaultProps}
+                action={actionElement}
+            />
+        )
+        const actionButton = screen.getByText(/Action Button/i)
+        expect(actionButton).toBeInTheDocument()
+    })
+})
---
+++ storybook/stories/Container.stories.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+import { Container, ContainerProps } from '../../src'
+
+import { Meta, StoryFn } from '@storybook/react'
+
+const meta: Meta<ContainerProps> = {
+    title: 'Components/Container',
+    component: Container,
+    argTypes: {
+        title: { control: 'text' },
+        className: { control: 'text' },
+        action: { control: 'text' },
+        header: { control: 'text' },
+        children: { control: 'text' },
+        footer: { control: 'text' }
+    }
+}
+
+export default meta
+
+const Template: StoryFn<ContainerProps> = (args: ContainerProps) => <Container {...args} />
+
+// Default story
+export const Default = Template.bind({})
+Default.args = {
+    title: 'Container Title',
+    children: 'This is the main content of the container.',
+    footer: <div>Footer content goes here</div>
+}
+
+// Story with custom action
+export const WithAction = Template.bind({})
+WithAction.args = {
+    title: 'Container with Action',
+    children: 'This container includes a custom action button.',
+    action: <button>Click Me</button>
+}
+
+// Story with custom header
+export const CustomHeader = Template.bind({})
+CustomHeader.args = {
+    header: <div>Custom Header Content</div>,
+    children: 'This container has a custom header.',
+    footer: <div>Footer content goes here</div>
+}
+
+// Story with footer
+export const WithFooter = Template.bind({})
+WithFooter.args = {
+    title: 'Container with Footer',
+    children: 'This container includes a footer.',
+    footer: <div>Custom Footer Content</div>
+}
