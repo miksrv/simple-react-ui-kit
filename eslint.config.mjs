249c35a944e8126610977bd4d40092f5012876c6
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2025-10-04 18:13:59 -0700

    Updated node version from `20` to `22` in the CI/CD
    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
 
 import { defineConfig } from 'eslint/config'
 import tseslint from 'typescript-eslint'
